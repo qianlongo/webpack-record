a1eb987cd40540e7109aebe09e32dedc4be22f02
wangmin23 <wangmin23@meituan.com>
2017-12-09 23:47:20 +0800

    update: 使用多入口

--- base-study/webpack-demo/src/index.js
+++ base-study/webpack-demo/src/index.js
@@ -1,16 +1,16 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
+import printMe from './print'
 
 function component () {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  element.classList.add('hello')
 
-  var myIcon = new Image()
-  myIcon.src = Icon
-  element.appendChild(myIcon)
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   return element
 }
