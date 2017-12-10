6aaaa18586360c0b60bf05d134a9aba91f1330cd
wangmin23 <wangmin23@meituan.com>
2017-12-10 21:41:08 +0800

    update: 使用uglifyJsPlugin去除未使用的代码

--- base-study/webpack-demo/src/index.js
+++ base-study/webpack-demo/src/index.js
@@ -1,16 +1,13 @@
-import _ from 'lodash'
-import printMe from './print'
+import { cube } from './math'
 
 function component () {
-  var element = document.createElement('div')
+  var element = document.createElement('pre')
   var btn = document.createElement('button')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe
-
-  element.appendChild(btn)
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
 
   return element
 }
--- base-study/webpack-demo/webpack.config.js
+++ base-study/webpack-demo/webpack.config.js
@@ -1,12 +1,14 @@
+const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -14,11 +16,18 @@ module.exports = {
     publicPath: '/'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true
+  },
   plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new UglifyJSPlugin()
   ]
 }
