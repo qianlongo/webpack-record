bb6a59241d2d9f2ac1d6926ede759bbd0bd8b1d3
qianlongo <1162633511@qq.com>
2018-02-01 10:02:02 +0800

    update: webpack-menifest

--- base-study/webpack-demo/webpack.prod.js
+++ base-study/webpack-demo/webpack.prod.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack')
+const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.comon')
+
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 
 const publicPath = 'https://static.meituan.net/bs/xy/fe-creditcard/file/public/'
+const basePath = path.resolve(__dirname, './')
 
 module.exports = merge(common, {
   devtool: 'source-map',  
